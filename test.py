b6d7ae9297b2baa0657b32f0035ccc3e87604fd4
James Sullivan <jsullivan@cambridgecloudpartners.com>
2013-08-01 22:11:01 -0400

    make watch for apex change to re-run tests

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import gevent
 from gevent import monkey
-monkey.patch_all(httplib=True)
+monkey.patch_all()
 
 import json
 import requests
-from requests.auth import HTTPBasicAuth
 from simple_salesforce import Salesforce
 import sys
 from time import sleep
@@ -16,24 +15,34 @@ sf = Salesforce(username='',
                 security_token='',
                 sandbox=True)
 
-base_url = 'https://'+sf.sf_instance+\
+base_url = 'https://' + sf.sf_instance + \
            '/services/data/v28.0/sobjects/ApexTestQueueItem'
-headers = {"Authorization": "Bearer "+sf.session_id,
+headers = {"Authorization": "Bearer " + sf.session_id,
            'Content-type': 'application/json'}
 
+
 def _queue_test(class_id):
-    res = requests.post(base_url, data=json.dumps({"ApexClassId": class_id}),
-        headers=headers)
+    res = requests.post(base_url,
+                        data=json.dumps({"ApexClassId": class_id}),
+                        headers=headers)
+
     if 'Apex test class already enqueued' in res.content:
-        print "You probably have 10 copies of each test scheduled"
-        exit(1)
+        print "%s is already scheduled for testing, sleeping..." % class_id
+        print class_id
+        print res.content
+        sleep(10)
+        return _queue_test(class_id)
+
     return res.json()['id']
+
 
 def _query(soql):
     url = 'https://'+sf.sf_instance+'/services/data/v28.0/query/'
     params = {"q": soql}
     res = requests.get(url, headers=headers, params=params)
+    assert res.status_code == 200
     return res.json()['records']
+
 
 def _check_test_result(test_id):
     res = requests.get(base_url + '/' + test_id, headers=headers)
@@ -51,30 +60,58 @@ def _check_test_result(test_id):
                 print "*" * 78
                 print r['Message']
                 print r['StackTrace']
-                print "*" * 78                
+                print "*" * 78
         print test_id, res['ExtendedStatus']
         return True
 
-def test(class_id):
+
+def _test(class_id):
     test_id = _queue_test(class_id)
-    sleep_times = 0
-    while True:
+    for i in range(20):
+        gevent.sleep(i)
         if _check_test_result(test_id):
-            break
-        gevent.sleep(3)
-        sleep_times += 1
-        if sleep_times > 10:
-            gevent.sleep(3)
-        assert sleep_times < 20, "%s slept too many times" % class_id
+            return
+    print "the test for class_id %s took too long, exiting" % class_id
+
+
+def _last_apex_class_change():
+    last_soql = ("select SystemModstamp from ApexClass "
+                 "order by SystemModstamp desc limit 1")
+    res = _query(last_soql)
+    if res:
+        return res[0]['SystemModstamp']
+
+
+def _sleep_until_new_change(last):
+    for i in range(20):
+        sleep(2)
+        new_last = _last_apex_class_change()
+        if new_last != last:
+            return new_last
+    print "No new tests for too long, shutting down to save api calls"
+    exit()
+
 
 def class_ids(pattern):
-    for r in _query("select Id, Name from ApexClass where Name like '%s'" % pattern):
+    soql = ("select Id, Name, SystemModstamp "
+            "from ApexClass where Name like '%s'")
+    for r in _query(soql % pattern):
         yield r['Id']
+
+
+def _tests(pattern):
+    return [gevent.spawn(_test, class_id) for class_id in class_ids(pattern)]
+
+
+def process_forever(pattern):
+    last = _last_apex_class_change()
+    while True:
+        gevent.joinall(_tests(pattern))
+        last = _sleep_until_new_change(last)
+        print "Change detected, running again...."
+
 
 if __name__ == "__main__":
     pattern = '%test%' if len(sys.argv) < 2 else sys.argv[1]
     print 'Running tests matching "%s"' % pattern
-    threads = []
-    for class_id in class_ids(pattern):
-        threads.append(gevent.spawn(test, class_id))
-    gevent.joinall(threads)
+    process_forever(pattern)
